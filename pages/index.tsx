efda49ee63522c5321ab77ccc253021ebbc4b244
wanderson almeida <wandersonalmeida.eng@gmail.com>
2023-09-22 10:14:02 -0300

    feat: implement material-ui in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,55 @@
 import { FormEvent, useContext, useState } from "react";
-import {setCookie} from 'nookies'
+import { Button, TextField } from "@material-ui/core";
 import { AuthContext } from "../contexts/AuthContext";
 
 export default function Home() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const {isAuthenticated, signIn} = useContext(AuthContext);
-   
-    async function handleSubmit(event: FormEvent) {
-        event.preventDefault()
-        const data = {
-            email,
-            password,
-        }
+  const { isAuthenticated, signIn } = useContext(AuthContext);
 
-       await signIn(data);
-    }
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = {
+      email,
+      password,
+    };
 
+    await signIn(data);
+  }
 
-    return(
-        <form onSubmit={handleSubmit} >
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            <button type="submit">Entrar</button>
-        </form>
-       
-    )
+  return (
+    <form 
+    style={{display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem',
+    width: '100%',
+    maxWidth: '20rem',
+    margin: '0 auto'}}
+
+    onSubmit={handleSubmit}>
+      <TextField
+      style={{width: '100%', display: "flex"}}
+        variant="outlined"
+        label="E-mail"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        style={{width: '100%'}}
+        variant="outlined"
+        label="Senha"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button variant="contained" color="secondary" type="submit">
+        Entrar
+      </Button>
+    </form>
+  );
 }
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios, {AxiosError} from 'axios';
 import {parseCookies} from 'nookies'
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
     baseURL: 'http://localhost:3333',
@@ -9,4 +9,37 @@ export const api = axios.create({
         Authorization: `Bearer ${cookies['nextauth.token']}`
     } 
 });
+
+api.interceptors.response.use(response => {
+    return response;
+}, error => {
+    if(error.response.status === 401) {
+        if(error.response.data?.code === 'token.expired') {
+           cookies = parseCookies();
+
+           const { 'nextauth.refreshToken': refreshToken } = cookies;
+
+           api.post('/refresh', {
+                refreshToken,
+              }).then(response => {
+                const { token } = response.data;
+    
+                cookies = parseCookies();
+    
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+                cookies['nextauth.token'] = token;
+    
+                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+                cookies['nextauth.refreshToken'] = response.data.refreshToken;
+    
+              }).catch(err => {
+                console.log(err);
+           })
+
+        } else {
+        }
+    }
+
+    return Promise.reject(error);
+})
 
